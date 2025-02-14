bc4098582d060b4079eebedf51dae90b6633d231
ereiskind <ereiskind@fsu.edu>
2025-02-14 15:06:37 -0500

    Add custom logger

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -1,9 +1,22 @@
 import sys
+import logging
 from awsglue.utils import getResolvedOptions
+
+def configure_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format= "[%(asctime)s] %(name)s::%(lineno)d - %(message)s",  # "[timestamp] module name::line number - error message"
+        datefmt="%Y-%m-%d %H:%M:%S",
+        encoding="utf-8",
+    )
+
+log = logging.getLogger(__name__)
 
 args = getResolvedOptions(sys.argv, [
     "name",
     "arg2",
 ])
 
+log.info("Start logging")
 print(f"Hello, {args['name']}!")
+log.info("End logging")
