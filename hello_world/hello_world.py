be9149205dd0cfb970fe0c4dfe8677fc88a92e77
ereiskind <ereiskind@fsu.edu>
2025-02-25 10:16:45 -0500

    Output individual logger dicts

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -16,18 +16,13 @@ def configure_logging():
     print(f"`logging.root.manager.__dict__` at end of function:\n{logging.root.manager.__dict__}")
 
 name_log = logging.getLogger(__name__)
-#TEST:print(f"`logging.root.manager.loggerDict['name_log']`: {logging.root.manager.loggerDict['name_log']}\n{logging.root.manager.loggerDict['name_log'].__dict__}")
-print(f"`logging.root.manager.loggerDict` after `name_log`: {logging.root.manager.loggerDict}")
+print(f"`logging.root.manager.loggerDict['__main__']`: {logging.root.manager.loggerDict['__main__']}\n{logging.root.manager.loggerDict['__main__'].__dict__}")
 configure_logging()
 test1_log = logging.getLogger("test1_log")
-print(f"`test1_log`: {test1_log}\n{test1_log.__dict__}")
-print(f"`logging.root.manager.loggerDict` after `test1_log`: {logging.root.manager.loggerDict}")
-#TEST:print(f"`logging.root.manager.loggerDict['test1_log']`: {logging.root.manager.loggerDict['test1_log']}\n{logging.root.manager.loggerDict['test1_log'].__dict__}")
+print(f"`logging.root.manager.loggerDict['test1_log']`: {logging.root.manager.loggerDict['test1_log']}\n{logging.root.manager.loggerDict['test1_log'].__dict__}")
 logging.basicConfig(level=logging.DEBUG)
 test2_log = logging.getLogger("test2_log")
-print(f"`test2_log`: {test2_log}\n{test2_log.__dict__}")
-print(f"`logging.root.manager.loggerDict` after `test2_log`: {logging.root.manager.loggerDict}")
-#TEST:print(f"`logging.root.manager.loggerDict['test2_log']`: {logging.root.manager.loggerDict['test2_log']}\n{logging.root.manager.loggerDict['test2_log'].__dict__}")
+print(f"`logging.root.manager.loggerDict['test2_log']`: {logging.root.manager.loggerDict['test2_log']}\n{logging.root.manager.loggerDict['test2_log'].__dict__}")
 
 args = getResolvedOptions(sys.argv, ["name"])
 
